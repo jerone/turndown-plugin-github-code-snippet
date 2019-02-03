18f9962ee9f8874475b7e058328f3e82fa097b99
Jeroen van Warmerdam <jeronevw@hotmail.com>
2019-02-03 21:22:56 +0100

    Wrap with UMD

--- turndown-plugin-github-code-snippet.js
+++ turndown-plugin-github-code-snippet.js
@@ -1,10 +1,26 @@
-function turndownPluginGithubCodeSnippet(turndownService) {
-	return turndownService.addRule("code-snippet", {
-		filter: function (node, options) {
-			return node.nodeName.toUpperCase() === "DIV" && !!node.querySelector('a[href^="https://github.com"][href*="#L"]');
-		},
-		replacement: function (content, node, options) {
-			return node.querySelector('a[href^="https://github.com"][href*="#L"]').getAttribute('href');
-		}
-	});
-}
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		// Node, CommonJS-like
+		module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.returnExports = factory();
+	}
+}(this, function () {
+
+	function turndownPluginGithubCodeSnippet(turndownService) {
+		return turndownService.addRule("code-snippet", {
+			filter: function (node, options) {
+				return node.nodeName.toUpperCase() === "DIV" && !!node.querySelector('a[href^="https://github.com"][href*="#L"]');
+			},
+			replacement: function (content, node, options) {
+				return node.querySelector('a[href^="https://github.com"][href*="#L"]').getAttribute('href');
+			}
+		});
+	}
+
+	return turndownPluginGithubCodeSnippet;
+}));
