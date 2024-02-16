99b80fc79764cf607eb98813e56fdfdc86f6da54
Jeroen van Warmerdam <jeronevw@hotmail.com>
2024-02-16 15:42:47 +0100

    Prettier

--- src/turndown-plugin-github-code-snippet.js
+++ src/turndown-plugin-github-code-snippet.js
@@ -1,26 +1,30 @@
 (function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define([], factory);
-	} else if (typeof exports === 'object') {
-		// Node, CommonJS-like
-		module.exports = factory();
-	} else {
-		// Browser globals (root is window)
-		root.turndownPluginGitHubCodeSnippet = factory();
-	}
-}(this, function () {
+  if (typeof define === "function" && define.amd) {
+    // AMD
+    define([], factory);
+  } else if (typeof exports === "object") {
+    // Node, CommonJS-like
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.turndownPluginGitHubCodeSnippet = factory();
+  }
+})(this, function () {
+  function turndownPluginGitHubCodeSnippet(turndownService) {
+    return turndownService.addRule("code-snippet", {
+      filter: function (node, options) {
+        return (
+          node.nodeName.toUpperCase() === "DIV" &&
+          !!node.querySelector('a[href^="https://github.com"][href*="#L"]')
+        );
+      },
+      replacement: function (content, node, options) {
+        return node
+          .querySelector('a[href^="https://github.com"][href*="#L"]')
+          .getAttribute("href");
+      },
+    });
+  }
 
-	function turndownPluginGitHubCodeSnippet(turndownService) {
-		return turndownService.addRule("code-snippet", {
-			filter: function (node, options) {
-				return node.nodeName.toUpperCase() === "DIV" && !!node.querySelector('a[href^="https://github.com"][href*="#L"]');
-			},
-			replacement: function (content, node, options) {
-				return node.querySelector('a[href^="https://github.com"][href*="#L"]').getAttribute('href');
-			}
-		});
-	}
-
-	return turndownPluginGitHubCodeSnippet;
-}));
+  return turndownPluginGitHubCodeSnippet;
+});
--- test/turndown-plugin-github-code-snippet-test.js
+++ test/turndown-plugin-github-code-snippet-test.js
@@ -1,13 +1,13 @@
-var Attendant = require('turndown-attendant');
-var TurndownService = require('turndown');
-var turndownPluginGitHubCodeSnippet = require('../src/turndown-plugin-github-code-snippet.js');
+var Attendant = require("turndown-attendant");
+var TurndownService = require("turndown");
+var turndownPluginGitHubCodeSnippet = require("../src/turndown-plugin-github-code-snippet.js");
 
 var attendant = new Attendant({
-	file: __dirname + '/index.html',
-	TurndownService: TurndownService,
-	beforeEach: function(turndownService) {
-		turndownService.use(turndownPluginGitHubCodeSnippet);
-	}
+  file: __dirname + "/index.html",
+  TurndownService: TurndownService,
+  beforeEach: function (turndownService) {
+    turndownService.use(turndownPluginGitHubCodeSnippet);
+  },
 });
 
 attendant.run();
